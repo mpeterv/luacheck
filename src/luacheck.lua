8c199f730f150565a8bbcd29f037fb29089ad6c4
mpeterv <petjamelnik@yandex.ru>
2014-05-31 13:34:39 +0400

    Updated API docs in comments

--- src/luacheck.lua
+++ src/luacheck.lua
@@ -3,15 +3,17 @@ local get_report = require "luacheck.get_report"
 --- Checks files with given options. 
 -- `files` should be an array of paths or a single path. 
 -- Recognized options:
--- `options.check_global` - should luacheck check for global access? Default: true. 
--- `options.check_redefined` - should luacheck check for redefined locals? Default: true. 
--- `options.check_unused` - should luacheck check for unused locals? Default: true. 
--- `options.check_unused_args` - should luacheck check for unused arguments and iterator variables? Default: true. 
--- `options.check_unused_value` - should luacheck check for unused values? Default: true. 
--- `options.globals` - set of standard globals. Default: _G. 
--- `options.env_aware` - ignore globals is chunks with custom _ENV. Default: true. 
--- `options.ignore` - set of variables to ignore. Default: empty. Takes precedense over `options.only`. 
--- `options.only` - set of variables to report. Default: report all. 
+--    `options.check_global` - should luacheck check for global access? Default: true. 
+--    `options.check_redefined` - should luacheck check for redefined locals? Default: true. 
+--    `options.check_unused` - should luacheck check for unused locals? Default: true. 
+--    `options.check_unused_args` - should luacheck check for unused arguments and
+--        iterator variables? Default: true. 
+--    `options.check_unused_value` - should luacheck check for unused values? Default: true. 
+--    `options.globals` - set of standard globals. Default: _G. 
+--    `options.env_aware` - ignore globals is chunks with custom _ENV. Default: true. 
+--    `options.ignore` - set of variables to ignore. Default: empty. 
+--       Takes precedense over `options.only`. 
+--    `options.only` - set of variables to report. Default: report all. 
 -- 
 -- Returns report. 
 -- Report is an array of file reports. 
@@ -23,11 +25,13 @@ local get_report = require "luacheck.get_report"
 -- And other fields except `file` will be absent. 
 --
 -- Warning is a table with several fields. 
--- `type` field may contain "global", "redefined" or "unused". 
--- "global" is for accessing non-standard globals. 
--- "redefined" is for redefinition of a local in the same scope, e.g. `local a; local a`. 
--- "unused" is for unused locals. 
--- `subtype` field may contain "read" or "write" for `global` type and "loop" or "arg" or "var" for other types. 
+-- `type` field may contain "global", "redefined", "unused" or "unused_value". 
+--    "global" is for accessing non-standard globals. 
+--    "redefined" is for redefinition of a local in the same scope, e.g. `local a; local a`. 
+--    "unused" is for unused locals. 
+--    "unused_value" is for unused values assigned to locals. 
+-- `subtype` field may contain "access" or "set" for `global` type and "loop", 
+--    "arg", "vararg" or "var" for other types. 
 -- `name` field contains the name of problematic variable. 
 -- `line` field contains line number where the problem occured. 
 -- `column` field contains offest of the name in that line. 
@@ -35,7 +39,8 @@ local get_report = require "luacheck.get_report"
 -- `prev_line` field contains line number where the variable was previously defined. 
 -- `prev_column` field contains offest of the variable name in that line. 
 --
--- The global report contains global counter of warnings per type in its `global`, `redefined` and `unused` fields. 
+-- The global report contains global counter of warnings per type in its `global`, `redefined`, 
+--    `unused` and `unused_value` fields. 
 -- `total` field contains total number of warnings in all files. 
 -- And `errors` field contains total number of errors. 
 local function luacheck(files, options)
