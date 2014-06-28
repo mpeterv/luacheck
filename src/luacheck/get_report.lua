b75054aa8c2d05e260a6689cddeacca6221cd68a
mpeterv <petjamelnik@yandex.ru>
2014-06-28 14:04:17 +0400

    Allow checking stdin by passing `-` as filename

--- src/luacheck/get_report.lua
+++ src/luacheck/get_report.lua
@@ -6,27 +6,26 @@ local luaparser = luacompiler.new()
 -- Returns a file report. 
 -- See luacheck function. 
 local function get_report(file, options)
-   local ok, source = pcall(function()
-      local handler = io.open(file, "rb")
-      local source = assert(handler:read("*a"))
-      handler:close()
-      return source
-   end)
+   local filename = file == "-" and "stdin" or file
+   local src
 
-   if not ok then
-      return {error = "I/O", file = file}
+   if not pcall(function()
+         local handler = file == "-" and io.stdin or io.open(file, "rb")
+         src = assert(handler:read("*a"))
+         handler:close() end) then
+      return {error = "I/O", file = filename}
    end
 
    local ast
-   ok, ast = pcall(function() return luaparser:src_to_ast(source) end)
 
-   if not ok then
-      return {error = "syntax", file = file}
-   else
-      local report = check(ast, options)
-      report.file = file
-      return report
+   if not pcall(function()
+         ast = luaparser:src_to_ast(src) end) then
+      return {error = "syntax", file = filename}
    end
+
+   local report = check(ast, options)
+   report.file = filename
+   return report
 end
 
 return get_report
