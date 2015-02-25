2b5525a7a8e0e3ca647f77099a6189d7a26cd4fc
mpeterv <mpeterval@gmail.com>
2015-02-25 11:08:32 +0300

    Update commands for installing say on Lua 5.3

--- .travis_setup_rocks.sh
+++ .travis_setup_rocks.sh
@@ -1,9 +1,8 @@
 # A script for setting up broken Lua rocks for travis-ci testing for Lua 5.3. 
 
 if [ "$LUA" == "Lua 5.3" ]; then
-  git clone https://github.com/mpeterv/say
+  git clone https://github.com/Olivine-Labs/say
   cd say
-  git checkout lua-53-compat
   sudo luarocks make say-1.2-1.rockspec
   cd ..
 
