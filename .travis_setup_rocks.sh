e3925553271389478e7471976b91d8ad9d1605e3
mpeterv <mpeterval@gmail.com>
2015-02-27 16:40:10 +0300

    Install dev version of busted

--- .travis_setup_rocks.sh
+++ .travis_setup_rocks.sh
@@ -14,3 +14,8 @@ cd lanes
 git checkout lua53-fixes
 sudo luarocks make lanes-3.9.6-1.rockspec
 cd ..
+
+git clone https://github.com/Olivine-Labs/busted
+cd busted
+sudo luarocks make busted-scm-0.rockspec
+cd ..
