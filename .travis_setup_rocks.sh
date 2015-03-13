3418fbef684406bb948ef5d392317f5232dcbf6b
mpeterv <mpeterval@gmail.com>
2015-03-13 13:48:26 +0300

    Update travis setup

--- .travis_setup_rocks.sh
+++ .travis_setup_rocks.sh
@@ -1,13 +1,6 @@
-# A script for setting up broken Lua rocks for travis-ci testing. 
+# A script for setting up broken Lua rocks for travis-ci testing.
 
-if [ "$LUA" == "Lua 5.3" ]; then
-  git clone https://github.com/Olivine-Labs/say
-  cd say
-  sudo luarocks make say-1.2-1.rockspec
-  cd ..
-
-  sudo luarocks install dkjson --deps-mode=none
-fi
+sudo luarocks install dkjson --deps-mode=none
 
 git clone https://github.com/mpeterv/lanes
 cd lanes
