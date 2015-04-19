a954a2acaa85ce5e64bc510ca1ec58285334fe62
mpeterv <mpeterval@gmail.com>
2015-04-19 20:34:01 +0300

    Add coveralls on travis

--- .travis_setup_rocks.sh
+++ .travis_setup_rocks.sh
@@ -1,4 +1,4 @@
-# A script for setting up broken Lua rocks for travis-ci testing.
+# A script for setting up broken/scm Lua rocks for travis-ci testing.
 
 sudo luarocks install dkjson --deps-mode=none
 
@@ -12,4 +12,10 @@ mkdir busted
 wget -O - https://api.github.com/repos/Olivine-Labs/busted/tarball/master | tar xz -C busted --strip-components=1
 cd busted
 sudo luarocks make busted-scm-0.rockspec
+cd ..
+
+mkdir luacov
+wget -O - https://api.github.com/repos/mpeterv/luacov/tarball/pause-resume | tar xz -C luacov --strip-components=1
+cd luacov
+sudo luarocks make rockspecs/luacov-scm-1.rockspec
 cd ..
