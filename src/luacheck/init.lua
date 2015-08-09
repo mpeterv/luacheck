4973151120941ce063e2a2c339054c9004faf97c
mpeterv <mpeterval@gmail.com>
2015-08-09 19:36:58 +0300

    0.11.1 release

--- src/luacheck/init.lua
+++ src/luacheck/init.lua
@@ -5,7 +5,7 @@ local format = require "luacheck.format"
 local utils = require "luacheck.utils"
 
 local luacheck = {
-   _VERSION = "0.11.0"
+   _VERSION = "0.11.1"
 }
 
 local function raw_validate_options(fname, opts)
