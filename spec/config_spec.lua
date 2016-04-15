3c1b12820d9e15af28f4888c2bfe7852ce8d6a76
mpeterv <mpeterval@gmail.com>
2016-04-15 23:23:48 +0300

    Windows compat fixes for config tests

--- spec/config_spec.lua
+++ spec/config_spec.lua
@@ -1,23 +1,26 @@
 local config = require "luacheck.config"
 local fs = require "luacheck.fs"
+local P = fs.normalize
 local cur_dir = fs.has_lfs and fs.lfs.currentdir()
 
 local function nest(dir, func)
-   if fs.has_lfs then
-      local backed = false
-
-      local function back()
-         if not backed then
-            fs.lfs.chdir(cur_dir)
-            backed = true
-         end
-      end
-
-      finally(back)
-      fs.lfs.chdir(dir)
-      func()
-      back()
+   if not fs.has_lfs then
+      pending("uses lfs")
    end
+
+   local backed = false
+
+   local function back()
+      if not backed then
+         fs.lfs.chdir(cur_dir)
+         backed = true
+      end
+   end
+
+   finally(back)
+   fs.lfs.chdir(dir)
+   func()
+   back()
 end
 
 describe("config", function()
@@ -33,11 +36,11 @@ describe("config", function()
          local nested_conf = config.load_config()
          assert.is_table(nested_conf)
          assert.same(config.get_top_options(conf), config.get_top_options(nested_conf))
-         assert.same(config.get_options(conf, "spec/foo.lua"), config.get_options(nested_conf, "../foo.lua"))
-         assert.equal("../../bar.lua", config.relative_path(nested_conf, "bar.lua"))
+         assert.same(config.get_options(conf, P"spec/foo.lua"), config.get_options(nested_conf, P"../foo.lua"))
+         assert.equal(P"../../bar.lua", config.relative_path(nested_conf, "bar.lua"))
       end)
 
-      assert.not_same(config.get_options(conf, "spec/foo_spec.lua"), config.get_options(conf, "foo_spec.lua"))
+      assert.not_same(config.get_options(conf, P"spec/foo_spec.lua"), config.get_options(conf, "foo_spec.lua"))
       assert.equal("bar.lua", config.relative_path(conf, "bar.lua"))
    end)
 
@@ -50,72 +53,86 @@ describe("config", function()
    end)
 
    it("loads config from path", function()
-      local conf = config.load_config("spec/configs/override_config.luacheckrc")
+      local conf = config.load_config(P"spec/configs/override_config.luacheckrc")
       assert.is_table(conf)
 
       nest("spec/configs/project", function()
-         local nested_conf = config.load_config("spec/configs/override_config.luacheckrc")
+         local nested_conf = config.load_config(P"spec/configs/override_config.luacheckrc")
          assert.is_table(nested_conf)
          assert.same(config.get_top_options(conf), config.get_top_options(nested_conf))
-         assert.same(config.get_options(conf, "spec/samples/bad_code.lua"), config.get_options(nested_conf, "../../samples/bad_code.lua"))
-         assert.equal("../../../bar.lua", config.relative_path(nested_conf, "bar.lua"))
+         assert.same(
+            config.get_options(conf, P"spec/samples/bad_code.lua"),
+            config.get_options(nested_conf, P"../../samples/bad_code.lua")
+         )
+         assert.equal(P"../../../bar.lua", config.relative_path(nested_conf, "bar.lua"))
       end)
 
-      assert.not_same(config.get_options(conf, "spec/samples/bad_code.lua"), config.get_options(conf, "spec/samples/unused_code.lua"))
+      assert.not_same(
+         config.get_options(conf, P"spec/samples/bad_code.lua"),
+         config.get_options(conf, P"spec/samples/unused_code.lua")
+      )
       assert.equal("bar.lua", config.relative_path(conf, "bar.lua"))
    end)
 
    it("returns nil, error on missing config", function()
-      local conf, err = config.load_config("spec/configs/config_404.luacheckrc")
+      local conf, err = config.load_config(P"spec/configs/config_404.luacheckrc")
       assert.is_nil(conf)
-      assert.equal("Couldn't find configuration file spec/configs/config_404.luacheckrc", err)
+      assert.equal("Couldn't find configuration file "..P"spec/configs/config_404.luacheckrc", err)
    end)
 
    it("returns nil, error on config with bad syntax", function()
-      local conf, err = config.load_config("spec/configs/bad_config.luacheckrc")
+      local conf, err = config.load_config(P"spec/configs/bad_config.luacheckrc")
       assert.is_nil(conf)
-      assert.matches("Couldn't load configuration from spec/configs/bad_config.luacheckrc: syntax error %(line 2: .*%)", err)
+      assert.matches("Couldn't load configuration from "..P"spec/configs/bad_config.luacheckrc"..
+         ": syntax error %(line 2: .*%)", err)
 
       nest("spec/configs/project", function()
-         local nested_conf, nested_err = config.load_config("spec/configs/bad_config.luacheckrc")
+         local nested_conf, nested_err = config.load_config(P"spec/configs/bad_config.luacheckrc")
          assert.is_nil(nested_conf)
-         assert.matches("Couldn't load configuration from ../../../spec/configs/bad_config.luacheckrc: syntax error %(line 2: .*%)", nested_err)
+         assert.matches("Couldn't load configuration from "..P"../../../spec/configs/bad_config.luacheckrc"..
+            ": syntax error %(line 2: .*%)", nested_err)
       end)
    end)
 
    it("returns nil, error on config with runtime issues", function()
-      local conf, err = config.load_config("spec/configs/runtime_bad_config.luacheckrc")
+      local conf, err = config.load_config(P"spec/configs/runtime_bad_config.luacheckrc")
       assert.is_nil(conf)
-      assert.equal("Couldn't load configuration from spec/configs/runtime_bad_config.luacheckrc: runtime error (line 1: attempt to call a nil value)", err)
+      assert.equal("Couldn't load configuration from "..P"spec/configs/runtime_bad_config.luacheckrc"..
+         ": runtime error (line 1: attempt to call a nil value)", err)
 
       nest("spec/configs/project", function()
-         local nested_conf, nested_err = config.load_config("spec/configs/runtime_bad_config.luacheckrc")
+         local nested_conf, nested_err = config.load_config(P"spec/configs/runtime_bad_config.luacheckrc")
          assert.is_nil(nested_conf)
-         assert.equal("Couldn't load configuration from ../../../spec/configs/runtime_bad_config.luacheckrc: runtime error (line 1: attempt to call a nil value)", nested_err)
+         assert.equal("Couldn't load configuration from "..P"../../../spec/configs/runtime_bad_config.luacheckrc"..
+            ": runtime error (line 1: attempt to call a nil value)", nested_err)
       end)
    end)
 
    it("returns nil, error on invalid config", function()
-      local conf, err = config.load_config("spec/configs/invalid_config.luacheckrc")
+      local conf, err = config.load_config(P"spec/configs/invalid_config.luacheckrc")
       assert.is_nil(conf)
-      assert.equal("Couldn't load configuration from spec/configs/invalid_config.luacheckrc: invalid value of option 'ignore'", err)
+      assert.equal("Couldn't load configuration from "..P"spec/configs/invalid_config.luacheckrc"..
+         ": invalid value of option 'ignore'", err)
 
       nest("spec/configs/project", function()
-         local nested_conf, nested_err = config.load_config("spec/configs/invalid_config.luacheckrc")
+         local nested_conf, nested_err = config.load_config(P"spec/configs/invalid_config.luacheckrc")
          assert.is_nil(nested_conf)
-         assert.equal("Couldn't load configuration from ../../../spec/configs/invalid_config.luacheckrc: invalid value of option 'ignore'", nested_err)
+         assert.equal("Couldn't load configuration from "..P"../../../spec/configs/invalid_config.luacheckrc"..
+            ": invalid value of option 'ignore'", nested_err)
       end)
    end)
 
    it("returns nil, error on config with invalid override", function()
-      local conf, err = config.load_config("spec/configs/invalid_override_config.luacheckrc")
+      local conf, err = config.load_config(P"spec/configs/invalid_override_config.luacheckrc")
       assert.is_nil(conf)
-      assert.equal("Couldn't load configuration from spec/configs/invalid_override_config.luacheckrc: invalid value of option 'enable' in options for path 'spec/foo.lua'", err)
+      assert.equal("Couldn't load configuration from "..P"spec/configs/invalid_override_config.luacheckrc"..
+         ": invalid value of option 'enable' in options for path 'spec/foo.lua'", err)
 
       nest("spec/configs/project", function()
-         local nested_conf, nested_err = config.load_config("spec/configs/invalid_override_config.luacheckrc")
+         local nested_conf, nested_err = config.load_config(P"spec/configs/invalid_override_config.luacheckrc")
          assert.is_nil(nested_conf)
-         assert.equal("Couldn't load configuration from ../../../spec/configs/invalid_override_config.luacheckrc: invalid value of option 'enable' in options for path 'spec/foo.lua'", nested_err)
+         assert.equal("Couldn't load configuration from "..P"../../../spec/configs/invalid_override_config.luacheckrc"..
+            ": invalid value of option 'enable' in options for path 'spec/foo.lua'", nested_err)
       end)
    end)
 end)
