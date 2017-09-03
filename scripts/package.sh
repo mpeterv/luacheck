5bfb01a03d8a5fb9f9370750b4b2a966882c502b
Peter Melnichenko <mpeterval@gmail.com>
2017-09-03 20:55:13 +0300

    Add packaging script

---
+++ scripts/package.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+set -eu
+set -o pipefail
+
+# Creates rockspec and source rock for a new Luacheck release given version number.
+# Should be executed from root Luacheck directory.
+# Resulting rockspec and rock will be in `package/`.
+
+version="$1"
+
+rm -rf package
+mkdir package
+cd package
+
+
+echo
+echo "=== Creating rockspec for Luacheck $version ==="
+echo
+
+luarocks new-version ../luacheck-scm-1.rockspec --tag="$version"
+
+echo
+echo "=== Copying Luacheck files ==="
+echo
+
+mkdir luacheck
+cp -r ../src luacheck
+mkdir luacheck/bin
+cp ../bin/luacheck.lua luacheck/bin
+cp -r ../doc luacheck
+cp ../README.md ../CHANGELOG.md ../LICENSE luacheck
+
+echo
+echo "=== Packing source rock for Luacheck $version ==="
+echo
+
+zip -r luacheck-"$version"-1.src.rock luacheck luacheck-"$version"-1.rockspec
+
+cd ..
