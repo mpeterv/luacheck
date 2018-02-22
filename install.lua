c6fe157cf58e8a69ffb4f6276e1224382b40fa7f
Kyle Evans <kevans91@ksu.edu>
2018-02-21 23:37:46 -0600

    Sprinkle $(realpath ...) invocations for symlink-ability
    At the moment, trying to install luacheck into /usr/local/lua/5.2/luacheck and
    then symlinking /usr/local/lua/5.2/lua/bin/luacheck into /usr/local/bin results
    in a failure as luacheck tries to locate its sources with respect to where the
    symlink lives.
    
    Add some $(realpath ...) to get the real location of luacheck for discovery of
    its sources.

--- install.lua
+++ install.lua
@@ -76,7 +76,7 @@ else
    fh:write(([=[
 #!/bin/sh
 exec "%s" -e "package.path=[[%s/../src/?.lua;%s/../src/?/init.lua;]]..package.path" "%s/luacheck.lua" "$@"
-]=]):format(args.lua, '$(dirname "$0")', '$(dirname "$0")', '$(dirname "$0")'))
+]=]):format(args.lua, '$(dirname $(realpath "$0"))', '$(dirname $(realpath "$0"))', '$(dirname $(realpath "$0"))'))
 end
 
 fh:close()
